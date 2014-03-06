1c9a8d617df7ff4399b110958ec6a270114a08e7
ed <ed.cinemascop@yahoo.es>
2014-03-06 19:14:24 -0200

    add util

--- util.c
+++ util.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-#include <ext2.h>
 
 struct mem_tag {
   void* next;
   int size;
 };
+
+typedef struct bit_test {
+  unsigned int a : 1;
+  unsigned int b : 2;
+} bit_test_t;
 
 long pow(long n, int exponent) {
   long ret = n;
@@ -32,16 +36,10 @@ void putint(int val) {
 
 int main() {
 
-  FILE *f;
-  f = fopen("disk.img.bak", "rb");
-  fseek(f, 1024, SEEK_SET);
-  char *superblock = (char*)malloc(1024);
-  fread(superblock, 1, 1024, f);
-  ext2_fs_info_t *i = ext2_parse_superblock(superblock);
-  printf("sys info:\ninodes:%d\nblocks:%d\nunallocated inodes:%d\nunallocated blocks:%d\n", i->total_inodes, i->total_blocks, i->unallocated_inodes, i->unallocated_blocks);
-  printf("blocks per group:%d\ninodes per group:%d\n", i->blocks_per_group, i->inodes_per_group);
-  printf("signature: 0x%x\nuid:%d\ngid:%d\n", i->signature, i->superuser_id, i->superuser_groupid);
-  fclose(f);
-  free(superblock);
+  printf("size: %d\n", sizeof(struct bit_test));
+  bit_test_t test;
+  test.a = 1;
+  test.b = 2;
+  printf("content: 0x%x\n", test);
   return 0;
 }
